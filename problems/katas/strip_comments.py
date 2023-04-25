2c0de22a4d2d25877b96e53d4186205f3e96049c
Ilya Novikau <ilya.nvu@gmail.com>
2023-04-25 21:48:56 +0300

    Add problem description

--- problems/katas/strip_comments.py
+++ problems/katas/strip_comments.py
@@ -1,3 +1,8 @@
+"""Complete the solution so that it strips all text that follows any of a set of
+comment markers passed in. Any whitespace at the end of the line should also be
+stripped out. """
+
+
 def strip_comments(string: str, markers: list[str]) -> str:
     i = 0
     stack = []
