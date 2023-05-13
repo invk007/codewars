17960488560af8cbb49ddc73aa3429a5e8597799
Ilya Novikau <ilya.nvu@gmail.com>
2023-05-13 22:59:37 +0300

    Simplify unit test

--- tests/problems/stacks/test_next_greater_element.py
+++ tests/problems/stacks/test_next_greater_element.py
@@ -1,10 +1,5 @@
-import pytest
-
 from problems.stacks.next_greater_element import next_greater_element
 
 
-@pytest.mark.parametrize("nums1, nums2, expected", [
-    ([4, 1, 2], [1, 3, 4, 2], [-1, 3, -1]),
-])
-def test_next_greater_element(nums1, nums2, expected):
-    assert next_greater_element(nums1, nums2) == expected
+def test_next_greater_element():
+    assert next_greater_element([4, 1, 2],  [1, 3, 4, 2]) == [-1, 3, -1]
