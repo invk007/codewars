c3f118f0810c7a2ae30a7f8ec903e8fc5c8629d4
Ilya Novikau <ilya.nvu@outlook.com>
2023-05-20 11:37:22 +0300

    Unit tests for missing number problem

--- problems/hashing/missing_number.py
+++ problems/hashing/missing_number.py
@@ -2,22 +2,15 @@
 return the only number in the range that is missing from the array."""
 
 
-def find_missing_number(nums: list[int]) -> int:
+def find_missing_num(nums: list[int]) -> int:
     n = len(nums)
     expected_sum = n * (n + 1) / 2
     return int(expected_sum - sum(nums))
 
 
-def find_missing_number_additional_space(nums: list[int]) -> int:
+def find_missing_num_add_space(nums: list[int]) -> int:
     present_nums = set(nums)
     for i in range(len(nums) + 1):
         if i not in present_nums:
             return i
     return -1
-
-
-if __name__ == '__main__':
-    assert 8 == find_missing_number([6, 1, 2, 7, 9, 4, 0, 3, 5])
-    assert 8 == find_missing_number_additional_space(
-        [6, 1, 2, 7, 9, 4, 0, 3, 5]
-    )
